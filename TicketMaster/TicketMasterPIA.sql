b73b203fc7952fa4c5ede32690f6c630728fcc18
Johann-28 <johann.velazquezantn@uanl.edu.mx>
2023-05-25 22:34:20 -0600

    Añadiendo creacion de schema

---
+++ TicketMaster/TicketMasterPIA.sql
@@ -0,0 +1,4 @@
+# ----------------------------------------------------------------------------- CREACION DE SCHEMA ------------------------------------------------------------------------------- #
+DROP SCHEMA IF EXISTS ticket_master;
+CREATE SCHEMA ticket_master;
+USE ticket_master;
